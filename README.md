028295363e485c0ec2a0887eeeba5413b9c189f5
TanishaShrotriya96 <tanishashrotriya91@gmail.com>
2021-10-02 12:03:36 -0500

    Final Code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 npm install node@16.6.1 --save-exact
+
+git commands - 
+git clone apiHack
+git checkout <branch_name> 
+branch_name can be discord_api or trigger_api
+git add <file1>,<file2>..
+git commit -m "write short info about features"
+git push origin <branch_name>
+
+**Rename master branch to main from github GUI
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 require('dotenv').config(); //initialize dotenv
+const request = require('request');
+
 
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const Discord = require('discord.js');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES,Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.on("ready", () => {
+	console.log(`logged in as ${client.user.tag}`);
+});
+  
+//Listen to new messages on the server
+client.on("messageCreate", (msg) => {
+	if (msg.content.toLowerCase() === "i want to kill myself") {
+	  msg.channel.send({content:"Hey! Seems like you're having a hard time. You can call some helpline numbers - 800-273-8255"});
+	};
+	if(msg.content === "no" || msg.content === "i don't want to call anyone") {
+		msg.channel.send({content:"That's okay. May I make the call for you?"});
+	}
+	
+	if(msg.content.toLowerCase() === "alright" || msg.content.toLowerCase() === "yes" || msg.content.toLowerCase() === "ok" ) {
+		msg.channel.send({content:"Great, a professional will be on their way to make sure you are okay."});
+		msg.channel.send({content: "Please watch this calming video till then.\n https://www.youtube.com/watch?v=O-6f5wQXSu8"})
+	}
+
 });
 
-// Login to Discord with your client's token
+
+ // Login to Discord with your client's token
 client.login(process.env.CLIENT_TOKEN); //login bot using token
